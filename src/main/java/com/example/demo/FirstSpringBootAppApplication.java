6b2844156d692c877372c6d91e2ca97fd513662f
Tudor <tudorcristianmurariu@yahoo.com>
2022-09-12 15:47:43 +0300

    database stuff, not working yet

--- src/main/java/com/example/demo/FirstSpringBootAppApplication.java
+++ src/main/java/com/example/demo/FirstSpringBootAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FristSpringBootAppApplication {
+public class FirstSpringBootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FristSpringBootAppApplication.class, args);
+		SpringApplication.run(FirstSpringBootAppApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/demo/FirstSpringBootAppApplicationTests.java
+++ src/test/java/com/example/demo/FirstSpringBootAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FristSpringBootAppApplicationTests {
+class FirstSpringBootAppApplicationTests {
 
 	@Test
 	void contextLoads() {
